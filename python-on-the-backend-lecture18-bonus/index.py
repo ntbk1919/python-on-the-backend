2aebb85bd045c2e5617085e47cefb497d4cacb65
ntbk1919 <60788087+ntbk1919@users.noreply.github.com>
2020-07-18 10:20:14 +0530

    Update index.py

--- python-on-the-backend-lecture18-bonus/index.py
+++ python-on-the-backend-lecture18-bonus/index.py
@@ -3,19 +3,19 @@ import tornado.ioloop
 
 class uploadImgHandler(tornado.web.RequestHandler):
     def post(self):
-        files = self.request.files["fileImage"]
+        files = self.request.files["imgFile"]
         for f in files:
-            fh = open(f"upload/{f.filename}", "wb")
+            fh = open(f"img/{f.filename}", "wb")
             fh.write(f.body)
             fh.close()
-        self.write(f"http://localhost:8080/img/{f.filename}")
+        self.write(f"`http://localhost:8080/img/{f.filename}`")
     def get(self):
         self.render("index.html")
 
 if (__name__ == "__main__"):
     app = tornado.web.Application([
         ("/", uploadImgHandler),
-        ("/img/(.*)", tornado.web.StaticFileHandler, {'path': 'upload'})
+        ("/img/(.*)", tornado.web.StaticFileHandler, {'path': 'img'})
     ])
 
     app.listen(8080)
